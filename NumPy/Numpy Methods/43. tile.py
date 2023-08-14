00332f641826542ee544af16aac66a39f8b631e3
Dharni <dharni0806@gmail.com>
2023-08-14 18:56:12 +0530

    Day 4 :- Methods

--- NumPy/Numpy Methods/43. tile.py
+++ NumPy/Numpy Methods/43. tile.py
@@ -1,2 +1,11 @@
 import numpy as np
 
+arr = np.arange(5)
+print("arr : \n", arr)
+
+repetitions = 2
+print("Repeating arr 2 times : \n", np.tile(arr, repetitions))
+
+repetitions = 3
+print("\nRepeating arr 3 times : \n", np.tile(arr, repetitions))
+
