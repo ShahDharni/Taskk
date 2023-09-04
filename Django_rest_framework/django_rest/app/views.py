2271e68afd8fd193be459dadfad96cb1f8abfe36
Dharni <dharni0806@gmail.com>
2023-09-04 18:51:54 +0530

    Django Rest Framework

---
+++ Django_rest_framework/django_rest/app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Student
+from.serializers import StudentSerializer
+from rest_framework.response import Response
+
+# Create your views here.
+class StudentApi(APIView):
+    def get(self,request,pk=None,format=None):
+        id=pk
+        if id is None:
+            stu=Student.objects.get(id=id)
+            serializer= StudentSerializer(stu)
+            return Response(serializer.data)
+        stu=Student.objects.all()
+        serializer=StudentSerializer(stu,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request,format=None):
+        serializer= StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"Data Created"})
+
+
