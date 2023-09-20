54330d1f13ee6918e63a2a6eb82efd4a7eb1be23
Dharni <dharni0806@gmail.com>
2023-09-20 17:59:29 +0530

    Pandas Task

--- panda/Student_data/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Student
-from rest_framework import serializers
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Student
-        fields=('gender','race_ethnicity','Parental_level_of_education','lunch','test_preparation','math_score','reading_score','writing_score')
--- panda/Student_data/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from.views import StudentListCreateView
-
-urlpatterns = [
-    path("student/",StudentListCreateView.as_view())
-]
