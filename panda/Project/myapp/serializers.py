39ebc8c74eabbe7c36ae49c30191dc792e2b032e
Dharni <dharni0806@gmail.com>
2023-09-20 18:19:01 +0530

    Pandas Task

---
+++ panda/Project/myapp/serializers.py
@@ -0,0 +1,7 @@
+from .models import Student
+from rest_framework import serializers
+
+class StudentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Student
+        fields=("gender","race_ethnicity","Parental_level_of_education","lunch","test_preparation","math_score","reading_score","writing_score")
---
+++ panda/Project/myapp/views.py
@@ -0,0 +1,45 @@
+
+import pandas as pd
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Student 
+from .serializers import StudentSerializers 
+import datetime  
+
+class StudentListCreateView(APIView):
+    def get(self, request):
+        csv_file_path = 'C:/Users/BAPS/Desktop/Daily Task/panda/StudentsPerformance.csv'
+
+        try:
+            df = pd.read_csv(csv_file_path)
+        except FileNotFoundError:
+            return Response({"error": "CSV file not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        data = df.to_dict(orient='records')
+        print("Start", datetime.datetime.now())
+
+        for student_data in data:
+            keys = student_data.keys()
+            values = student_data.values()
+            print(keys)
+            print(values)
+        
+        students = Student.objects.all()
+        print("students",students)
+        print("End", datetime.datetime.now())
+
+        return Response(data)
+    
+    def post(self, request):
+        serializer = StudentSerializers(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()  
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+
+
+
+
+
